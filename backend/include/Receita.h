8785f63134d9ba63a195a543b58a4a4e533139f9
Heloisa Leite <heloisamsleite@gmail.com>
2026-09-25 12:06:55 -0300

    feat(financeiro): implementa estrutura base de transacoes

--- backend/include/Receita.h
+++ backend/include/Receita.h
@@ -1,1 +1,24 @@
-// Heloisa
+#ifndef _RECEITA_H_
+#define _RECEITA_H_
+
+#include <string>
+#include "transacao.h"
+using namespace std;
+
+class Receita : public transacao{
+    private:
+        string origem;
+    public:
+        Receita(int id, string descricao, double valor, string data, string origem ) : transacao(id, descricao, valor, data), origem(origem){
+
+        }
+        double ContribuicaoSaldo() const override {
+            return valor;
+        }
+        string getTipo() const override{
+            return "Receita";
+        }
+
+};    
+
+#endif
